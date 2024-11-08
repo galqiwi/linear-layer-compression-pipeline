c71364b780206c4017611c15f130551b7b10d5bb
Malinovskii Vladimir <galqiwi@gmail.com>
2024-11-08 16:26:14 +0300

    empty cache one time

--- slopes_eval/main.py
+++ slopes_eval/main.py
@@ -267,7 +267,8 @@ def get_model_hidden_states(model, dataloader, dev):
             inps[j] = layer(inps[j], attention_mask=attention_masks[j], position_ids=position_ids[j])[0]
         layers[i] = layer.to('meta')
         del layer
-        torch.cuda.empty_cache()
+
+    torch.cuda.empty_cache()
 
     hidden_states = inps
 
