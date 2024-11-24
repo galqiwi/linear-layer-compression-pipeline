1cc68872f7325a476b5de0d1100232557b354e32
Malinovskii Vladimir <galqiwi@gmail.com>
2024-11-24 11:02:53 +0300

    fixed parallel

--- var_bitrate_eval/edenn.py
+++ var_bitrate_eval/edenn.py
@@ -53,12 +53,18 @@ class HadLinear(nn.Module):
         super().__init__()
         self.register_buffer('had_block_size', torch.tensor(0))
         self.had_block_size = torch.tensor(had_block_size)
-        self.weight = nn.Parameter(weight/math.sqrt(had_block_size))
-    
+        weight = weight / math.sqrt(had_block_size)
+        out_dim, in_dim = weight.shape
+        self.inner = nn.Linear(in_features=in_dim, out_features=out_dim, bias=False, device='meta')
+        self.inner.weight = nn.Parameter(
+            weight,
+            requires_grad=False,
+        )
+
     def forward(self, input):
         input = pad_to_block(input, [-1], self.had_block_size)
         mult = input.shape[-1] // self.had_block_size
         input = input.reshape(input.shape[:-1] + (mult, self.had_block_size))
-        input = hadamard_transform(input, scale=1/math.sqrt(self.had_block_size))
+        input = hadamard_transform(input, scale=1 / math.sqrt(self.had_block_size))
         input = input.reshape(input.shape[:-2] + (mult * self.had_block_size,))
-        return F.linear(input, self.weight)
+        return self.inner(input)
--- var_bitrate_eval/main.py
+++ var_bitrate_eval/main.py
@@ -437,23 +437,8 @@ def main():
             ppl = llama_eval(model, testloader, DEV)
             wandb.log({f'ppl_{dataset}': ppl})
 
-    if torch.cuda.device_count() > 1:
-        # Inference of Llama2-70b on 3 80GB GPUs
-        import accelerate
-
-        n_devices = torch.cuda.device_count()
-
-        device_map = accelerate.infer_auto_device_map(model, max_memory={device_idx: "40GB" for device_idx in
-                                                                         range(n_devices)},
-                                                      no_split_module_classes=['LlamaDecoderLayer'])
-
-        print(device_map)
-
-        assert 'disk' not in set(device_map.values())
-
-        model = accelerate.dispatch_model(model, device_map=device_map)
-    else:
-        model = model.to(DEV)
+    from parallel import dispatch_model_parallel
+    model = dispatch_model_parallel(model)
 
     wandb.log(get_zero_shots(
         model,
